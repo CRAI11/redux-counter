0d0fc93a48b886a29522ffb1e37cd230e7acbe04
Chandan Rai <chank.raiii@gmail.com>
2021-08-26 19:52:14 +0530

    redux-counter is done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,31 +1,19 @@
-import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { increment, reset, decrement } from "../store/counter/actions";
 
-export default function Counter(props) {
+export default function Counter() {
   const dispatch = useDispatch();
 
   const counter = useSelector((state) => state.counter);
 
   console.log(counter);
-  // const [counter, setCounter] = useState(0);
-
-  // const increment = () => {
-  //   let count = counter;
-  //   count++;
-  //   setCounter(count);
-  // };
 
   const decFunction = () => {
     if (counter > 0) {
       dispatch(decrement());
     }
   };
-
-  // const reset = () => {
-  //   setCounter(0);
-  // };
 
   return (
     <>
